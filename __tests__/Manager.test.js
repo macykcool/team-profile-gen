82a83c79c93cedc29b7e58dbd0ac0ec6d4042960
Macy Kay Mannix <macymannix@gmail.com>
2022-05-20 19:59:40 -0700

    employee test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,14 @@
+const Employee = require('../lib/Employee');
+
+test('should allow you to enter a name, id and email', () => {
+    const empo = new Employee('Macy', 55, 'macykcool@gmail.com');
+
+    expect(empo.name).toBe('Macy');
+    expect(empo.id).toBe(55);
+    expect(empo.email).toBe('macykcool@gmail.com');
+    expect(empo.getName()).toBe('Macy');
+    expect(empo.getId()).toBe(55);
+    expect(empo.getEmail()).toBe('macykcool@gmail.com');
+    expect(empo.getRole()).toBe('Employee');
+
+});
