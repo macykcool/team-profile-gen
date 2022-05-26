13c25ced6db7bceb2f9f95347c049800d580e07d
Macy Kay Mannix <macymannix@gmail.com>
2022-05-25 20:08:27 -0700

    questions in LIB

--- index.js
+++ index.js
@@ -1,67 +1,129 @@
 const inquirer = require("inquirer");
 const generateHTML = require("./src/generateHTML.js");
 
-const promptTeam = [
-  //manager
-  {
-    type: "input",
-    name: "name",
-    message: "What is your name?",
-    validate: (nameInput) => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log("Please enter your name!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "Enter your employee ID",
-    validate: (nameInput) => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log("Please enter your employee ID!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Enter your employee email",
-    validate: (nameInput) => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log("Please enter your employee email address!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "office",
-    message: "Enter your office number",
-    validate: (nameInput) => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log("Please enter your office!");
-        return false;
-      }
-    },
-  },
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('.lib/Intern');
 
+// const promptMgr = () => {
+//   return inquirer
+//     .prompt([
+//   //manager name employee id, email, office number 
+//   {
+//     type: "input",
+//     name: "manager",
+//     message: "What is the Manager name for this project?",
+//     validate: (nameInput) => {
+//       if (nameInput) {
+//         return true;
+//       } else {
+//         console.log("Please enter manager name!");
+//         return false;
+//       }
+//     },
+//   },
+//   {
+//     type: "input",
+//     name: "manager id",
+//     message: "Enter manager employee ID",
+//     validate: (nameInput) => {
+//       if (nameInput) {
+//         return true;
+//       } else {
+//         console.log("Please enter manager employee ID!");
+//         return false;
+//       }
+//     },
+//   },
+//   {
+//     type: "input",
+//     name: "manager email",
+//     message: "Enter manager's email",
+//     validate: (nameInput) => {
+//       if (nameInput) {
+//         return true;
+//       } else {
+//         console.log("Please enter the manager's email address!");
+//         return false;
+//       }
+//     },
+//   },
+//   {
+//     type: "input",
+//     name: "office number",
+//     message: "Enter the manager's office number",
+//     validate: (nameInput) => {
+//       if (nameInput) {
+//         return true;
+//       } else {
+//         console.log("Please enter the manager's office!");
+//         return false;
+//       }
+//     },
+//     },
+//   ]);
+// };
+
+
+// const promptTeam = portfolioData => {
+//   if (!portfolioData.projects) {
+//     portfolioData.projects = [];
+//   } 
+//   return inquirer.prompt([
+//     {
+//       type: 'list',
+//       name: 'employee add',
+//       message: 'Please select an employee classification to add to this project',
+//       //change choice type and have to submit one only?
+//       choices: ['Engineer', 'Intern', 'All done']
+//       },
+
+//       //if engineer, add the enginner and bring back to menu
+//       //done building when ALL DONE selected 
+//     // {
+//     //   type: 'confirm',
+//     //   name: 'confirmEngineer',
+//     //   message: 'Would you like to add an Engineer to this project?',
+//     //   default: true
+//     // },
+//     // {
+//     //   type: 'input',
+//     //   name: 'engineer',
+//     //   message: 'Select the Engineer',
+//     //   when: ({ confirmEngineer }) => {
+//     //     if (confirmEngineer) {
+//     //       return true;
+//     //     } else {
+//     //       return false;
+//     //     }
+//     //   }
+//     // },
+//     // {
+//     //   type: 'confirm',
+//     //   name: 'confirmIntern',
+//     //   message: 'Would you like to add an Intern to this project?',
+//     //   default: true
+//     // },
+//     // {
+//     //   type: 'input',
+//     //   name: 'intern',
+//     //   message: 'Select the Intern',
+//     //   when: ({ confirmIntern }) => {
+//     //     if (confirmIntern) {
+//     //       return true;
+//     //     } else {
+//     //       return false;
+//     //     }
+//     //   }
+//     // },
+//   ]);
+// };
   //would you like to add an engineer or intern or finish
 
   //engineer
 
   //intern
-];
+
 
 //finish
 
@@ -70,7 +132,8 @@ const promptTeam = [
 // function that writes html with css and template
 
 function init() {
-  inquirer.prompt(promptTeam).then(function (userInput) {
+  inquirer.prompt(promptMgr)
+  .then(function (userInput) {
     console.log(userInput);
     writeToFile("./dist/index.html", generateHTML(userInput));
   });
